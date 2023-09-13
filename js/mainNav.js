1ac27ded2c2c80064e8f3fdb264acae072bf0ac0
rdoriol <rdoriol@gmail.com>
2023-09-13 19:10:33 +0200

    nav finished

--- js/mainNav.js
+++ js/mainNav.js
@@ -2,47 +2,76 @@
  * Fichero javascript/JQuery para dotar de funcionalidad a la barra lateral de navegación principal.
  */
 
-$(document).ready(function() {
-                                                console.log(sessionStorage.getItem("submenuShow"));
-                                                console.log(sessionStorage.getItem("test"));
+$(document).ready(function() {                                  
+                                 
     /**
      * Función que abrirá y cerrará los submenus al pulsar los botones del menú.
      */
     $("ul li button").click(function(e) {
-        var display = $(this).next().css("display"); // se obtiene valor de la propiedad "display" del button.
+        var buttonId = $(this).attr("id");
+        var submenuId = $(this).next().attr("id");            
 
-        if(display == "block") {
+        if($(this).next().css("display") == "block") {
+            $(this).next().slideUp();
             navBarReset();
-                                                sessionStorage.setItem("submenuShow", "false");
+            sessionStorage.setItem("submenuShowStore", "false");     // almacena en sesión de navegador el estado del menú.
         }
         else {
             navBarReset();
-            $(this).next().slideDown(); // abre submenus.
-            $(this).addClass("li_active"); // añade clase para que menú seleccionado aparezca con las propiedades css de botón activo.
-            $(this).children().attr("class", "fa-solid fa-caret-down"); // se cambia a icono "submenú expandido".
-                                                sessionStorage.setItem("submenuShow", "true");
-                                                sessionStorage.setItem("test", $(this).next().value);
-        }     
-                                                
-    })  
+            $(this).next().slideDown();                                     // abre submenus.
+            $(this).addClass("li_active");                                  // añade clase para que menú seleccionado aparezca con las propiedades css de botón activo.
+            $(this).children().attr("class", "fa-solid fa-caret-down");     // se cambia a icono "submenú expandido".
+               
+                // bloque para almacenar los "id" y estado del menú en sesión del navegador.
+            sessionStorage.setItem("submenuShowStore", "true");
+            sessionStorage.setItem("submenuIdStore", submenuId);
+            sessionStorage.setItem("buttonIdStore", buttonId);
+        }                                         
+    })    
 
     /**
-     * Función que marcará submenús seleccionados con propiedades css de boton activo.
+     * Función que marcará opciones de los submenús seleccionados con propiedades css de boton activo.
      */
     $("ul li ul li a").click(function(e) {
         $(".link_subButton").removeClass("link_subButton");
-        $(this).addClass("link_subButton");
+       // $(this).addClass("link_subButton_active"); de momento no utilizar
+
+        sessionStorage.setItem("aIdStore", $(this).attr("id"));
     })
 
     /**
      * Función que reseteará a valores iniciales los menús desplegables.
      */
     function navBarReset() {
-        $(".submenu").slideUp(); // cierra todos los submenus previamente abiertos.
-        $(".link_button").removeClass("li_active");  // elimina clase con las propiedades css de botón activo de los menús seleccionados previamente.
-        $(".fa-solid").attr("class", "fa-solid fa-caret-right"); // cambia todos los iconos al tipo "submenú contraido".
+        $(".submenu").slideUp();                                    // cierra todos los submenus previamente abiertos.
+        $(".link_button").removeClass("li_active");                 // elimina clase con las propiedades css de botón activo de los menús seleccionados previamente.
+        $(".fa-solid").attr("class", "fa-solid fa-caret-right");    // cambia todos los iconos al tipo "submenú contraido".
     }
 
+    /**
+     * Función que devolverá al estado en el que se encuentraba el menú lateral antes de refrescar página (abierto-cerrado, marcado-no marcado).
+     */
+    function reloadNavBar(e) {
+        var reloadSubmenuShow = sessionStorage.getItem("submenuShowStore");
+        var reloadSubmenuIdStore = sessionStorage.getItem("submenuIdStore");
+        var reloadButtonIdStore = sessionStorage.getItem("buttonIdStore");
+        var reloadAIdStore = sessionStorage.getItem("aIdStore");
+
+        if(reloadSubmenuShow != "undefined" && reloadSubmenuShow != null) {
+            if(reloadSubmenuShow == "true") {
+                $("#" + reloadSubmenuIdStore).show();
+                $("#" + reloadButtonIdStore).addClass("li_active");
+                $("#" + reloadButtonIdStore).children().attr("class", "fa-solid fa-caret-down");
+                $("#" + reloadAIdStore).addClass("link_subButton_active");
+            }
+        }
+    }
+
+    reloadNavBar();
+
+    
+
+ 
 
 
 })
--- views/base.template.php
+++ views/base.template.php
@@ -55,60 +55,60 @@
               <button role="link" class="link_button" id="index_button" onClick="window.location='index.php'">inicio</button> <!--  DAR UNA VUELTA AL TEMA DE LINK INICIO -->
             </li>
             <li class="li_item" id="">
-              <button class="link_button" title="Abrir opciones clientes"><i class="fa-solid fa-caret-right"></i>&nbsp Clientes</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=01-newCustomer">Alta cliente</a></li>
-                <li class=""><a class="" href="index.php?pages=02-customerFile">Ficha cliente</a></li>
-                <li class=""><a class="" href="index.php?pages=04-customersList">Listado clientes</a></li>
+              <button class="link_button" id="customers_button" title="Abrir opciones clientes"><i class="fa-solid fa-caret-right"></i>&nbsp Clientes</button>
+              <ul class="submenu" id="customers_submenu">
+                <li class="link_subButton"><a class="" id="newCustomerId" href="index.php?pages=01-newCustomer">Alta cliente</a></li>
+                <li class="link_subButton"><a class="" id="customerFileId" href="index.php?pages=02-customerFile">Ficha cliente</a></li>
+                <li class="link_subButton"><a class="" id="customersListId" href="index.php?pages=04-customersList">Listado clientes</a></li>
               </ul>
             </li>
             
             <li class="li_item" id="">
-              <button class="link_button"><i class="fa-solid fa-caret-right"></i>&nbsp Proveedores</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=01-newSupplier">Alta proveedor</a></li>
-                <li class=""><a class="" href="index.php?pages=02-supplierFile">Ficha proveedor</a></li>
-                <li class=""><a class="" href="index.php?pages=04-suppliersList">Listado proveedores</a></li>
+              <button class="link_button" id="suppliers_button" title="Abrir opciones proveedores"><i class="fa-solid fa-caret-right"></i>&nbsp Proveedores</button>
+              <ul class="submenu" id="suppliers_submenu">
+                <li class="link_subButton"><a class="" id="newSupplierId" href="index.php?pages=01-newSupplier">Alta proveedor</a></li>
+                <li class="link_subButton"><a class="" id="supplierFileId" href="index.php?pages=02-supplierFile">Ficha proveedor</a></li>
+                <li class="link_subButton"><a class="" id="suppliersListId" href="index.php?pages=04-suppliersList">Listado proveedores</a></li>
               </ul>
             </li>
 
             <li class="li_item" id="">
-              <button class="link_button"> <i class="fa-solid fa-caret-right"></i>&nbsp Gestión Inventario</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=01-newProduct">Alta producto</a></li>
-                <li class=""><a class="" href="index.php?pages=02-productFile">Ficha producto</a></li>
-                <li class=""><a class="" href="index.php?pages=04-productsInputs">Entradas productos</a></li>
-                <li class=""><a class="" href="index.php?pages=05-productsList">Listado productos</a></li>
+              <button class="link_button" id="inventory_button" title="Abrir opciones gestión inventario"> <i class="fa-solid fa-caret-right"></i>&nbsp Gestión Inventario</button>
+              <ul class="submenu" id="inventory_submenu">
+                <li class="link_subButton"><a class="" id="newProductId" href="index.php?pages=01-newProduct">Alta producto</a></li>
+                <li class="link_subButton"><a class="" id="productFileId" href="index.php?pages=02-productFile">Ficha producto</a></li>
+                <li class="link_subButton"><a class="" id="productsInputsId" href="index.php?pages=04-productsInputs">Entradas productos</a></li>
+                <li class="link_subButton"><a class="" id="productsListId" href="index.php?pages=05-productsList">Listado productos</a></li>
               </ul>
             </li>
 
             <li class="li_item" id="">
-              <button class="link_button"><i class="fa-solid fa-caret-right"></i>&nbsp Ventas</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=01-newOrder">Alta pedido</a></li>
-                <li class=""><a class="" href="index.php?pages=02-notes">Albaranes</a></li>
-                <li class=""><a class="" href="index.php?pages=03-invoice">Facturas</a></li>
-                <li class=""><a class="" href="index.php?pages=04-proInvoice">Facturas proforma</a></li>
-                <li class=""><a class="" href="index.php?pages=05-budgets">Presupuestos</a></li>
-                <li class=""><a class="" href="index.php?pages=06-salesLists">Listado ventas</a></li>
+              <button class="link_button" id="sales_button" title="Abrir opciones ventas"><i class="fa-solid fa-caret-right"></i>&nbsp Ventas</button>
+              <ul class="submenu" id="sales_submenu">
+                <li class="link_subButton"><a class="" id="newOrderId" href="index.php?pages=01-newOrder">Alta pedido</a></li>
+                <li class="link_subButton"><a class="" id="notesId" href="index.php?pages=02-notes">Albaranes</a></li>
+                <li class="link_subButton"><a class="" id="invoiceId" href="index.php?pages=03-invoice">Facturas</a></li>
+                <li class="link_subButton"><a class="" id="proInvoiceId" href="index.php?pages=04-proInvoice">Facturas proforma</a></li>
+                <li class="link_subButton"><a class="" id="budgetsId" href="index.php?pages=05-budgets">Presupuestos</a></li>
+                <li class="link_subButton"><a class="" id="salesListsId" href="index.php?pages=06-salesLists">Listado ventas</a></li>
               </ul>
             </li>
 
             <li class="li_item" id="">
-              <button class="link_button"><i class="fa-solid fa-caret-right"></i>&nbsp Empleados</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=01-newEmployee">Alta empleado</a></li>
-                <li class=""><a class="" href="index.php?pages=02-employeeFile">Ficha empleado</a></li>
-                <li class=""><a class="" href="index.php?pages=04-employeesList">Listado empleados</a></li>
+              <button class="link_button" id="employees_button" title="Abrir opciones empleados"><i class="fa-solid fa-caret-right"></i>&nbsp Empleados</button>
+              <ul class="submenu" id="employees_submenu">
+                <li class="link_subButton"><a class="" id="newEmployeeId" href="index.php?pages=01-newEmployee">Alta empleado</a></li>
+                <li class="link_subButton"><a class="" id="employeeFileId" href="index.php?pages=02-employeeFile">Ficha empleado</a></li>
+                <li class="link_subButton"><a class="" id="employeesListId" href="index.php?pages=04-employeesList">Listado empleados</a></li>
               </ul>
             </li>
 
             <li class="li_item" id="">
-              <button class="link_button"><i class="fa-solid fa-caret-right"></i>&nbsp Gestión Interna</button>
-              <ul class="submenu">
-                <li class=""><a class="" href="index.php?pages=00-users">Usuarios</a></li>
-                <li class=""><a class="" href="index.php?pages=00-inventorySetting">Configuración productos </a></li>
-                <li class=""><a class="" href="index.php?pages=00-salesSetting">Configuración ventas</a></li>
+              <button class="link_button" id="internal_button" title="Abrir opciones gestión interna"><i class="fa-solid fa-caret-right"></i>&nbsp Gestión Interna</button>
+              <ul class="submenu" id="internal_submenu">
+                <li class="link_subButton"><a class="" id="usersId" href="index.php?pages=00-users">Usuarios</a></li>
+                <li class="link_subButton"><a class="" id="inventorySettingId" href="index.php?pages=00-inventorySetting">Opciones productos </a></li>
+                <li class="link_subButton"><a class="" id="salesSettingId" href="index.php?pages=00-salesSetting">Opciones ventas</a></li>
               </ul>
             </li>
 
