514e09bbb529e3da35027191c65f4625bdc2fc0f
rdoriol <rdoriol@gmail.com>
2023-11-28 10:26:51 +0100

    finished

--- views/00-base.template.php
+++ views/00-base.template.php
@@ -10,7 +10,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>LD Soft Gestión</title>   
+    <title>LD SoftGestión</title>   
     
     <!-- RESET CSS DE NAVEGADORES
         ---------------------->
